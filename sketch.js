d41578b83e7728bf44586f0af77177efd9064a5e
simba4temylimbsahh <69889981+simba4temylimbsahh@users.noreply.github.com>
2020-12-26 11:21:55 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var r1, rb, r2
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+    
+
+
+	bottomSprite = createSprite(width/2, 650, 200, 10 )
+    bottomSprite.shapeColor = "red"
+
+	leftSprite = createSprite(500, 600, 10, 100)
+	leftSprite.shapeColor = "red"
+
+	rightSprite = createSprite(300, 600, 10, 100)
+    rightSprite.shapeColor = "red"
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+	 rb = Bodies.rectangle(width/2, 640, 100, 10, {isStatic:true})
+	 World.add(world, rb)
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  
+ 
+
+  drawSprites();
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+Matter.Body.setStatic(packageBody, false)
+    
+  }
+}
+
+
+
